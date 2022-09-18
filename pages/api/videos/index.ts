5d8f2bd0f7ad3785e198f6d9f672294c5e66217d
Kousik Manna <45976284+imkousik9@users.noreply.github.com>
2022-09-18 09:53:11 +0530

    fix

--- pages/api/videos/index.ts
+++ pages/api/videos/index.ts
@@ -5,19 +5,15 @@ export default async function videosHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'GET') {
-    try {
-      const data = await prisma.video.findMany({
-        include: {
-          category: true
-        }
-      });
+  try {
+    const data = await prisma.video.findMany({
+      include: {
+        category: true
+      }
+    });
 
-      res.status(200).json(data);
-    } catch (error) {
-      return res.status(400).send(error.message);
-    }
+    res.status(200).json(data);
+  } catch (error) {
+    return res.status(400).send(error.message);
   }
-
-  return res.send('Method not allowed.');
 }
